bf3c2aaa214a38a0763142f2fd2ca006531e1068
RAFAELLAMATOS <rafaella.matos@gmail.com>
2023-11-10 09:49:46 -0300

    update test

--- calculadora.test.js
+++ calculadora.test.js
@@ -4,6 +4,7 @@ describe("Teste da função somar", () => {
   test("deveria somar dois números inteiros positivos ", () => {
     expect(somar(2, 3)).toBe(5);
     expect(somar(1, 0)).toBe(1);
+    expect(somar(10, 1)).toBe(11);
   });
 
   test("deveria somar dois números inteiros negativos", () => {
